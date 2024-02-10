cad7bfc00baa9d885c8e94cc6f249ee6fef1e71c
ishant1800 <ishanaryan45@gmail.com>
2024-02-10 12:36:09 +0530

    updated

---
+++ pro-hooks/src/App.jsx
@@ -0,0 +1,68 @@
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import './App.css';
+
+const LARGE_NUMBER = 1000000000;
+
+function App() {
+  const [value, setValue] = useState(0);
+  const [dark, setTheme] = useState(true);
+  const [themeName, setThemeName] = useState('dark');
+  const [currentList, setList] = useState([]);
+
+  const delayFunction = useCallback(() => {
+    console.log('Delay Function Ran');
+    return () => {
+      for (let index = 0; index < LARGE_NUMBER; index++) {}
+      return value + 2;
+    };
+  }, [value]);
+
+  const testFunction = useCallback(() => {
+    return [value * 3, value * 4];
+  }, [value]);
+
+  useEffect(() => {
+    console.log('Callback Function was called');
+  }, [testFunction]);
+
+  useEffect(() => {
+    if (dark) {
+      setThemeName('dark');
+    } else {
+      setThemeName('light');
+    }
+  }, [dark]);
+
+  const handleClick = () => {
+    setTheme(!dark);
+  };
+
+  const handleChangeValue = () => {
+    setValue(value + 1);
+  };
+
+  const handleList = () => {
+    setList(testFunction());
+  };
+
+  const styleTheme = {
+    backgroundColor: dark ? 'black' : '#ccc7c7',
+  };
+
+  return (
+    <div className="page" style={styleTheme}>
+      <button onClick={handleClick}>{themeName}</button>
+      <h1>{value}</h1>
+      <button onClick={handleChangeValue}>Change Value</button>
+      <button onClick={handleList}>Show List</button>
+      <h2>{delayFunction()}</h2>
+      <div>
+        {currentList.map((item, index) => {
+          return <h2 key={index}>{item}</h2>;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
